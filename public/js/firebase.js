6823a813daf1129053437ead3ab351c333daab87
Vividavis <251189@virtualwindow.co.za>
2025-11-01 10:40:39 +0200

    set up firebase

---
+++ public/js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgJwzT5qE6ssOjTH_F4JiYZCeIOKyPEbY",
+  authDomain: "semester2group4project.firebaseapp.com",
+  projectId: "semester2group4project",
+  storageBucket: "semester2group4project.firebasestorage.app",
+  messagingSenderId: "708735600105",
+  appId: "1:708735600105:web:3b4df14430381a8feff778"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
