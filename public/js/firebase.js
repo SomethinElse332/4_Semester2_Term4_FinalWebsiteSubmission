be371498bbd924a9bebd842ce7f9a7a8ea1e989d
SomethinElse332 <251193@virtualwindow.co.za>
2025-11-02 11:13:46 +0200

    copy main to this branch

---
+++ public/js/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDi-a8E71ILhPSZ8ehlRekt9UE296Ld8Hc",
+  authDomain: "consolecrusaders.firebaseapp.com",
+  projectId: "consolecrusaders",
+  storageBucket: "consolecrusaders.firebasestorage.app",
+  messagingSenderId: "61425832767",
+  appId: "1:61425832767:web:d27ea85ce5684fac3cb98a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
